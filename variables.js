8d4291eff24816ce01f0a5f19e4180e7f8afb595
kapil.raghuwanshi <kapil.raghuwanshi5@gmail.com>
2024-08-19 16:04:37 +0530

    learned variables

---
+++ variables.js
@@ -0,0 +1,26 @@
+let name = "Kapil Raghuwanshi"; // stmt - instruction
+
+console.log(name);
+
+let name1 = "Tech Monk"; // stmt - instruction
+
+// use only alpha numeric ot $ or _
+let -num = 50;
+
+console.log(nu_m);
+
+// reserved keywords
+let function = 50;
+
+// case sensitive
+
+let NAME = "rohan";
+
+console.log(NAME);
+
+
+let Name = "Neha";
+
+let NaMe = "bhoomika";
+
+console.table([name, NAME, Name, NaMe]);
